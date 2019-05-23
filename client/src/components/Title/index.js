75031822725ee816f2aa777f1973cf4720b628da
Connor F <connorfinnigan@gmail.com>
2019-05-22 22:04:56 -0500

    updating github link in header to this project repo

--- client/src/components/Title/index.js
+++ client/src/components/Title/index.js
@@ -6,7 +6,7 @@ function Title(props) {
     <span>
       <h1 className="title">LaunchPartner Coding Assignment</h1>
       <h3 className="subTitle">featuring Punch-Out!! for NES (1987)</h3>
-      <h6 className="subsubTitle">by Connor Finnigan (<i className="fab fa-github"></i><a href="https://github.com/cf512" target="_blank" rel="noopener noreferrer"> cf512</a>)</h6>
+      <h6 className="subsubTitle">by Connor Finnigan (<i className="fab fa-github"></i><a href="https://github.com/cf512/launchpartner" target="_blank" rel="noopener noreferrer"> cf512</a>)</h6>
     </span>
   )
 }
