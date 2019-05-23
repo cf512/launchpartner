6a2a077e69a4bb1b7f70cd9ce32fc74da7c19252
Connor F <connorfinnigan@gmail.com>
2019-05-22 21:58:11 -0500

    defining our routes

---
+++ routes/api/characters.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const characterController = require("../../controllers/characterController");
+
+// Matches with "/api/books"
+router.route("/")
+  .get(characterController.findAll)
+  .post(characterController.create);
+
+// Matches with "/api/books/:id"
+router
+  .route("/:id")
+  .get(characterController.findById)
+  .put(characterController.update)
+  .delete(characterController.remove);
+
+module.exports = router;
