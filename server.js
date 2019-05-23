d1c2468c41493591e7385a68dbac022903c4b5ca
Connor F <connorfinnigan@gmail.com>
2019-05-22 21:51:03 -0500

    config for database & routes, and fixing console warnings

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const path = require("path");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -13,12 +14,11 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // API routes
+app.use(routes);
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// Connect to the Mongo DB
+mongoose.set("useCreateIndex", true);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/punchoutDB", { useNewUrlParser: true });
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
