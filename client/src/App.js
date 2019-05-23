1cd80cb603141c1e167ef3f81b75e677a8b87f5e
Connor F <connorfinnigan@gmail.com>
2019-05-22 21:48:30 -0500

    moved Home to a pages folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import Home from "./components/Home";
+import Home from "./pages/Home";
 
 function App(){
   return (
