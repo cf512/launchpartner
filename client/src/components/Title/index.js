dd8cd5987e5a6f93e870e4155a0739ef09959c10
Connor F <connorfinnigan@gmail.com>
2019-05-29 10:57:17 -0500

    linking main title back to home for convenience

--- client/src/components/Title/index.js
+++ client/src/components/Title/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function Title(props) {
   return (
     <span>
-      <h1 className="title">LaunchPartner Coding Assignment</h1>
+      <h1 className="title"><a href="/">LaunchPartner Coding Assignment</a></h1>
       <h3 className="subTitle">featuring Punch-Out!! for NES (1987)</h3>
       <h6 className="subsubTitle">by Connor Finnigan (<i className="fab fa-github"></i><a href="https://github.com/cf512/launchpartner" target="_blank" rel="noopener noreferrer"> cf512</a>)</h6>
     </span>
