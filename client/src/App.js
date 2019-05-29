96b2e5f48e88c320aec55d61af1d4914d7bccd71
Connor F <connorfinnigan@gmail.com>
2019-05-29 10:55:30 -0500

    moving header component to main page and adding Edit component+route

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Title from './components/Title';
 import Home from "./pages/Home";
+import Edit from "./pages/Edit";
 
 function App(){
   return (
     <Router>
       <div>
+        <Title />
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route path="/edit/:id" component={ Edit } />
         </Switch>
       </div>
     </Router>
